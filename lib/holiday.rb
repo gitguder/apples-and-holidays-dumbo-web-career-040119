42d267f94744e78cc95495bc6039fb7c5307f439
Codey Pinero <codey.pinero@gmail.com>
2019-03-27 18:10:17 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -76,5 +76,5 @@ def all_holidays_with_bbq(holiday_hash)
     holidays.collect { |holiday, supplies| holiday_bbq << holiday if supplies.include?("BBQ") }
   end
   holiday_bbq.uniq
-
+  binding.pry
 end
