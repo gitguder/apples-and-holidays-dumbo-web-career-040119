c1a1d332c2de4f80cce4ea4708c9374f1fa9b0de
Codey Pinero <codey.pinero@gmail.com>
2019-03-27 18:01:08 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,7 +61,7 @@ def all_supplies_in_holidays(holiday_hash)
     
     puts "#{seasons.capitalize!}:"
     holidays.each do |holiday, supplies|
-      holiday_split = holiday.to_s.split("_").collect {|word| words = word.capitalize!}.join(" ")
+      holiday_split = holiday.to_s.split("_").collect {|word| word.capitalize!}.join(" ")
       binding.pry
 
       puts "  #{holiday_split}: #{supplies.join(", ")}"
